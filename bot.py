2981e19e4d8c00d35c4c7b8c6f46177087ecc5e3
Mikel Uranga <mikeluranga@sipay.es>
2018-06-07 15:12:58 +0200

    credentials from an external txt

--- bot.py
+++ bot.py
@@ -1,20 +1,28 @@
+
 import selenium
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import json
 
 chrome_options = Options()
-chrome_options.set_headless(headless=True)
+# chrome_options.set_headless(headless=True)
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=r'/usr/bin/chromedriver')
 driver.get("https://github.com/login")
 
 username = driver.find_element_by_id("login_field")
 password = driver.find_element_by_id("password")
 
-username.send_keys("username")
-password.send_keys("password")
+with open('data.txt') as json_file:  
+    data = json.load(json_file)
+    name = data['name']
+    passw = data['pass']
+
+username.send_keys(name)
+password.send_keys(passw)
 time.sleep(1)
 
 driver.find_element_by_name("commit").click()
 
 print ("Headless Chrome Initialized")
+# driver.quit()
