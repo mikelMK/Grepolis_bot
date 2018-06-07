dae2d275604323c46033d606c3a1cb7f91ebb288
Mikel Uranga <mikeluranga@sipay.es>
2018-06-07 18:27:40 +0200

    login v2

--- login.py
+++ login.py
@@ -1,4 +1,10 @@
 from tkinter import *
+import os
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import json
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -20,7 +26,7 @@ class Application(Frame):
         txt_passw = Entry(window,width=10)
         txt_passw.place(x=60, y=30)
 
-        login = Button(window, text="Start Bot", fg="green",command=self.say_hi)
+        login = Button(window, text="Start Bot", fg="green", command=self.say_hi)
         login.place(x=60, y=60)
 
         quit = Button(window, text="Stop Bot", fg="red",command=window.destroy)
@@ -28,6 +34,23 @@ class Application(Frame):
 
     def say_hi(self):
         print("hi there, everyone!")
+
+    def start_bot(self, user, passw):
+        chrome_options = Options()
+        # chrome_options.set_headless(headless=True)
+        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=r'/usr/bin/chromedriver')
+        driver.get("https://github.com/login")
+
+        username = driver.find_element_by_id("login_field")
+        password = driver.find_element_by_id("password")
+
+        username.send_keys(user)
+        password.send_keys(passw)
+        time.sleep(1)
+
+        driver.find_element_by_name("commit").click()
+        print ("Headless Chrome Initialized")
+
 
 window =Tk()
 window.geometry('180x130')
